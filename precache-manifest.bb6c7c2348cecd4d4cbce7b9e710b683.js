186eb55f4c7c180ba7e51a3deb95a6079a30ab58
dtphu0169 <dtphu0169@gmail.com>
2022-12-11 22:46:06 +0700

    Updates

---
+++ precache-manifest.bb6c7c2348cecd4d4cbce7b9e710b683.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0143486b0278de524c1e",
+    "url": "/ReactWeb/static/css/main.6e4edeb2.chunk.css"
+  },
+  {
+    "revision": "0143486b0278de524c1e",
+    "url": "/ReactWeb/static/js/main.0143486b.chunk.js"
+  },
+  {
+    "revision": "1b3f48c6de62d66542ce",
+    "url": "/ReactWeb/static/js/1.1b3f48c6.chunk.js"
+  },
+  {
+    "revision": "f1cfe46e851955b1c6cf",
+    "url": "/ReactWeb/static/js/2.f1cfe46e.chunk.js"
+  },
+  {
+    "revision": "fb4becd741c557a9fcd7",
+    "url": "/ReactWeb/static/js/runtime~main.fb4becd7.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/ReactWeb/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4786e334bcfc2340bb83b0e27183a31e",
+    "url": "/ReactWeb/index.html"
+  }
+];
