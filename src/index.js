c13f6314fce590b106dfeba83c854ee3c61c5c9f
dtphu0169 <dtphu0169@gmail.com>
2022-12-12 21:15:10 +0700

    fix router 6

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter as Router} from 'react-router-dom';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
